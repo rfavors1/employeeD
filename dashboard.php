64b9c12ecdda4fb76ae1fa43d3768bff930e4d8a
rfavors1 <rfavors09@gmail.com>
2016-09-23 12:08:11 -0400

    main page

--- dashboard.php
+++ dashboard.php
@@ -1,15 +1,17 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<title>Untitled Document</title>
+<title>Employee Dashboard</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
 <div id="dash">
-  <a id="add">Add Employee</a>
-  <a id="view">View Employees</a>
+  <h2>Employee Dashboard</h2>
+  <p><a id="add" href="/add.php">Add Employee</a></p>
+  <p><a id="view" href="/view.php">View Employees</a></p>
 </div>
-
 </body>
 </html>
