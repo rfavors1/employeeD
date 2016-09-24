f923e479a84065bd38221a5b617b53fe4d46f552
rfavors1 <rfavors09@gmail.com>
2016-09-23 21:15:51 -0400

    edit

--- edit.php
+++ edit.php
@@ -21,7 +21,15 @@ $Name = $Email = $Hire = "";
   if (empty($_GET["ID"])) {
      echo "<h2 class='error'>Employee ID is required.</h2>";
 	 echo '<p><a class="goback" href="view.php">Go Back</a></p>';
-  } 
+  } else {
+    $ID = validate($_GET["ID"]);
+	$ID = intval($_GET["ID"]);
+	if (!(is_int($ID)) {
+     echo "<h2 class='error'>Employee ID must be in number format.</h2>";
+	 echo '<p><a class="goback" href="view.php">Go Back</a></p>';	
+	} 
+	echo $ID;
+  }
 
 function validate($data) {
   $data = trim($data);
@@ -36,9 +44,9 @@ function validate($data) {
   <h3>Edit Employee</h3>
   <p>*All fields are required.</p>
   <form  method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-    <p>Name: <input type="text" size=35 name="Ename" value="$Name"><span class="error"> <?php echo $NameError;?></span></p>
-    <p>Email: <input type="text" size=50 name="Eemail" value="$Email"><span class="error"> <?php echo $EmailError;?></span></p>
-    <p>Hiring Date: <input type="date" name="Ehire" value="$Hire"><span class="error"> <?php echo $HireError;?></span></p>
+    <p>Name: <input type="text" size=35 name="Ename" value="<?php echo $Name?>"><span class="error"> <?php echo $NameError;?></span></p>
+    <p>Email: <input type="text" size=50 name="Eemail" value="<?php echo $Email?>"><span class="error"> <?php echo $EmailError;?></span></p>
+    <p>Hiring Date: <input type="date" name="Ehire" value="<?php echo $Hire?>"><span class="error"> <?php echo $HireError;?></span></p>
 	<p><input type="submit" value="Update"></p>
   </form>
 </div>
