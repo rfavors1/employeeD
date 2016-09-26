2493360e9d5eb9fe20533643c8e2010e840236e2
rfavors1 <rfavors09@gmail.com>
2016-09-26 17:07:36 -0400

    JS File

---
+++ j/js/employee.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+function validateEmail(email) {
+  var check = true;
+    var atpos = email.indexOf("@");
+    var dotpos = email.lastIndeemailOf(".");
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=email.length) {
+        check = false;
+    }
+	
+	return check;
+}
