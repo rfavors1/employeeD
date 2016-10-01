759aa2edf57b35daa895d20b1f33d58e0bd4cd5d
rfavors1 <rfavors09@gmail.com>
2016-10-01 12:42:55 -0400

    Employee PHP

--- a/employee.php
+++ a/employee.php
@@ -1,3 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Add Employee</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
 <?php
 
 //add new hire training dates when employee added
@@ -92,3 +102,5 @@ $db = substr($url["path"], 1);
 }
 
 ?>
+</body>
+</html>
