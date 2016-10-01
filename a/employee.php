ec29def72befab2366bc8af4ea528b3b28641726
rfavors1 <rfavors09@gmail.com>
2016-10-01 16:58:02 -0400

    Employee PHP

--- a/employee.php
+++ a/employee.php
@@ -46,7 +46,7 @@ $db = substr($url["path"], 1);
 		echo $date;
 		//date_add($date,date_interval_create_from_date_string($interval));
 		//echo "Date: " . $date;
-        $//sql = "INSERT INTO employee_training (id,employee_id,training_id,due_date,completed) VALUES ('','$id','$training_id','$date','0')";
+        //$sql = "INSERT INTO employee_training (id,employee_id,training_id,due_date,completed) VALUES ('','$id','$training_id','$date','0')";
 
         $link->query($sql)
 	}	
