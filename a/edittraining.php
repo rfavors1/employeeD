2aa55e425122bb37f50f22809da10c229966975c
rfavors1 <rfavors09@gmail.com>
2016-10-02 20:57:41 -0400

    Edit Trainings

--- a/edittraining.php
+++ a/edittraining.php
@@ -35,6 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") { //handles update
     $d = "due_" . $s;
 	$c = "complete_" . $s;
 	$dc = "dcomplete_" . $s;
+	echo $_POST["due_1"];
     $Due = validate($_POST["due_1"]);
 	echo "<script>alert($Due);</script>";
     $Complete = intval(validate($_POST[$c]));
