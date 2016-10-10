b1914345f8ea509d65ccef9ce3cdf2f91f28f85f
rfavors1 <rfavors09@gmail.com>
2016-10-10 19:36:41 -0400

    Dashboard

--- a/dashboard.php
+++ a/dashboard.php
@@ -19,7 +19,8 @@
     <img src="img/rightarrow.png" class="open">  	
   </div>  
   <ul class="menu">
-  <li><a href="add.php">Add Employee</a></li>
+  <li class="employee">EMPLOYEES</li>
+  <li class="elink"><a href="add.php">Add Employee</a></li>
   <li><a href="view.php">View Employee</a></li>  
   </ul>
 </div>
@@ -28,6 +29,9 @@
 </div>
 </div>
 <script>
+$(".employee").click(function(){
+  $(".elink").toggle();
+});
 $(".close").click(function(){
     $(".menu").css("display","none"); 
 	$(".close").css("display","none"); 
